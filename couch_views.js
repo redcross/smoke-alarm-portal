db960028df0c1c3af1bc519b4bd508cda238f89c
Mark Nyon <mark@grandkru.com>
2015-06-19 16:24:55 -0400

    Updated data

--- couch_views.js
+++ couch_views.js
@@ -1,3 +1,11 @@
+/* These are map() functions to be used in the browser.
+ * They can be accessed through the following URLs (browser or cURL)
+ * county-matchup:
+ * http://localhost:5984/selected_counties/_design/selected_counties/_view/county-matchup?key=%22Ada%22
+ * by-zip-code:
+ * http://localhost:5984/us_addresses/_design/us_addresses/_view/by-zip-code?key=%2260101%22
+ * Instructions for creating a view are in DATA.md
+
 /* us_addresses/by-zip-code
  *
  *  View used for zip
