befab526b097e39c62506cb5190161a07f994ebf
Karl Fogel <kfogel@red-bean.com>
2015-06-24 17:47:49 +0800

    Trim whitespace from input and handle ZIP+4 codes.
    This fixes issues #29 (whitespace) and #25 (ZIP+4).
    
    * routes/index.js: Sanitize user input, in the new stanza introduced
      in commit dffcf53ef.  Handle both 5-digit zip codes and 5+4 digits.
      Display an at least mildly comprehensible error to the user if their
      zip code doesn't seem valid.  Use `var` keyword everywhere, as I
      should have been doing before except I just learned about it.

--- routes/index.js
+++ routes/index.js
@@ -29,18 +29,57 @@ router.post('/', function(req, res, next) {
     var stateFromZip = null;   // remains null if no match
     var countyFromZip = null;  // remains null if no match
 
-    // Get values from request.
-    name = req.body.name;
-    street_address = req.body.street_address;
-    city = req.body.city;
-    state = req.body.state;
-    zip_5 = req.body.zip;
-    zip_4 = null; // we don't handle this yet
-    phone = req.body.phone;
-    may_text = req.body.permission_to_text;
+    // Trim and sanitize the request values.
+    //
+    // Note: we could augment String like so
+    //
+    //     String.prototype.trimAndSlim() {
+    //         return this.trim().replace(/\s+/g, ' ');
+    //     };
+    //
+    // and use that to declutter the code below.  But I'm not sure
+    // whether such augmentation is frowned on or not.  Advice from
+    // more experienced Javascript programmers welcome.  -Karl
+    var name = req.body.name.trim().replace(/\s+/g, ' ');
+    var street_address = req.body.street_address.trim().replace(/\s+/g, ' ');
+    var city = req.body.city.trim().replace(/\s+/g, ' ');
+    var state = req.body.state.trim().replace(/\s+/g, ' ');
+    var phone = req.body.phone.trim().replace(/\s+/g, ' ');
+    var may_text = req.body.permission_to_text.trim().replace(/\s+/g, ' ');
 
-    // Get desired county and state
-    console.log("DEBUG: the user-provided zip code is: '" + zip_5 + "'");
+    // Treat zip code specially.  For zip codes, we remove all
+    // internal spaces, since they can't possibly be useful.
+    var zip_received = req.body.zip.trim().replace(/\s+/g, '');
+    // This is the zip code we will actually store in the database.
+    // Our canonical form for storing zip codes is any of the following:
+    // "NNNNN" (a 5 digit string), "NNNNN-NNNN" (a string consisting
+    // of 5 digits, a hyphen, and 4 digits), or null.  No other forms
+    // are to be stored, at least not without changing this comment.
+    var zip_final = null;
+
+    // Parse 5-digit section and optional 4-digit section from the zip code.
+    var zip_5 = null;
+    var zip_4 = null;
+    var zip_re = /^([0-9][0-9][0-9][0-9][0-9]) *[-_+]{0,1} *([0-9][0-9][0-9][0-9]){0,1}$/g;
+    var zip_match = zip_re.exec(zip_received)
+    if (zip_match) {
+        if (zip_match.length < 2) {
+            console.log("ERROR: zip matched, but match grouping is somehow wrong,");
+            console.log("       which implies that the regexp itself is not right");
+            console.log("       (or our use of it isn't right).");
+        }
+        else {
+            zip_5 = zip_match[1];
+            console.log("DEBUG: found the 5-digit portion of the zip code: '" + zip_5 + "'");
+            if (zip_match.length == 3) {
+                zip_4 = zip_match[2];
+                console.log("DEBUG: found a 4-digit portion in the zip code: '" + zip_4 + "'");
+                zip_final = zip_5 + "-" + zip_4;
+            } else {
+                zip_final = zip_5;
+            }
+        }
+    }
 
     // Save the request data unconditionally.  Even if we can't
     // service the request -- or even if it contains some invalid
@@ -52,7 +91,7 @@ router.post('/', function(req, res, next) {
             street_address: street_address,
             city: city,
             state: state,
-            zip: zip_5,
+            zip: zip_final,
             phone: phone,
             may_text: may_text
         },
@@ -116,7 +155,15 @@ router.post('/', function(req, res, next) {
             if (matchedRegion) {
                 res.render('thankyou.jade', {region: matchedRegion});
             } else {
-                res.render('sorry.jade', {county: countyFromZip, state: stateFromZip, zip: zip_5});
+                if (zip_final) {
+                    var zip_for_display = zip_final;
+                } else {
+                    // A better way to handle this would be to display a sorry
+                    // page that discusses the invalidity of the zip code and
+                    // doesn't talk about anything else.  But this will do for now.
+                    var zip_for_display = "(INVALID ZIP CODE '" + zip_received + "')";
+                }
+                res.render('sorry.jade', {county: countyFromZip, state: stateFromZip, zip: zip_for_display});
             }
         });
     });
