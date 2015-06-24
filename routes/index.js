dffcf53ef731c3bbd16bcf1c1f389e2148c6d482
Karl Fogel <kfogel@red-bean.com>
2015-06-24 15:19:22 +0800

    Reorganize some code and comments.
    No functional change should result from this; it is just meant to
    prepare the ground for some later changes.
    
    * routes/index.js: Transfer request data to separate variables in what
      will later become a data sanitization block, e.g., for trimming
      whitespace, etc.  Update and move around some comments so they're
      accurate again (they haven't been for a while).  Format the console
      log of a database connection error consistently with other console
      logs, but note that this error detection is apparently not working
      right now anyway; issue #28 is filed about that.

--- routes/index.js
+++ routes/index.js
@@ -20,37 +20,52 @@ router.get('/sorry', function(req, res, next) {
 
 /* POST to the server */
 router.post('/', function(req, res, next) {
-    // Get values from request
+    // Databases we'll need.
     var requestDb = nano.use('smoke_alarm_requests');
     var addressDb = nano.use('us_addresses');
     var countyDb = nano.use('selected_counties');
-    // Determine the county of the Zip code
-    var zipToSelect = req.body.zip;
-    var matchedCounty = null;  // matched from user-provided zip code; remains null if no match
-    var matchedState = null;   // matched from user-provided zip code; remains null if no match
-    // Get desired county and state
-    console.log("DEBUG: the user-provided zip code is: '" + zipToSelect + "'");
 
-    // Save the data
+    // Things we derive from the user-provided zip code.
+    var stateFromZip = null;   // remains null if no match
+    var countyFromZip = null;  // remains null if no match
+
+    // Get values from request.
+    name = req.body.name;
+    street_address = req.body.street_address;
+    city = req.body.city;
+    state = req.body.state;
+    zip_5 = req.body.zip;
+    zip_4 = null; // we don't handle this yet
+    phone = req.body.phone;
+    may_text = req.body.permission_to_text;
+
+    // Get desired county and state
+    console.log("DEBUG: the user-provided zip code is: '" + zip_5 + "'");
+
+    // Save the request data unconditionally.  Even if we can't
+    // service the request -- or even if it contains some invalid
+    // data, such as an unknown zip code -- we still want to record
+    // that the person made the request.
     requestDb.insert(
         {
-            name: req.body.name,
-            street_address: req.body.street_address,
-            city: req.body.city,
-            state: req.body.state,
-            zip: req.body.zip,
-            phone: req.body.phone,
-            permission_to_text: req.body.permission_to_text,
+            name: name,
+            street_address: street_address,
+            city: city,
+            state: state,
+            zip: zip_5,
+            phone: phone,
+            may_text: may_text
         },
         'Request from ' + req.body.name + ' at ' +  new Date().toLocaleString(),
         function(err, body, header) {
             if (err) {
-                console.log(err.message);
+                console.log("ERROR: '" + err.message + "'");
                 return;
             }
         });
 
-    addressDb.view('us_addresses','by-zip-code', {key:zipToSelect}, function(error, results) {
+    // Derive state+county from zip code
+    addressDb.view('us_addresses','by-zip-code', {key:zip_5}, function(error, results) {
         if (error) {
             if (String(error).toLowerCase().indexOf("error happened in your connection") != -1) {
                 console.log("ERROR: " + error)
@@ -59,17 +74,18 @@ router.post('/', function(req, res, next) {
                 console.log("DEBUG: unrecognized error: " + error);
             }
         }
-        console.log("DEBUG: doc result from search on zip '" + zipToSelect + "': " + JSON.stringify(results));
+        console.log("DEBUG: doc result from search on zip '" + zip_5 + "': " + JSON.stringify(results));
         results.rows.forEach(function(doc) {
-            if (matchedCounty) {
+            if (countyFromZip) {
                 console.log("DEBUG: another state+county match found: '"
                            + doc.value[1] + "'+'" + doc.value[0] + "'");
             }
-            matchedCounty = doc.value[0].replace(" County", "");
-            matchedState = doc.value[1];
+            countyFromZip = doc.value[0].replace(" County", "");
+            stateFromZip = doc.value[1];
         });
 
-        console.log("DEBUG: county '" + matchedCounty + "' in state '" + matchedState + "' found");
+        console.log("DEBUG: county '" + countyFromZip + "' in state '" + stateFromZip + "' "
+                    + "derived from zip code '" + zip_5 + "'");
         var matchedRegion = null;
         // We have to match both county and state.  Counties
         // are not only not unique across states, they are not
@@ -79,7 +95,7 @@ router.post('/', function(req, res, next) {
         // Nebraska have a Greeley County.  Try submitting one
         // request with zip 67879 (Greeley County, Kansas) and
         // another with zip 68665 (Greeley County, Nebraska).
-        countyDb.view('selected_counties','county-matchup', {key: [matchedState,matchedCounty]}, function(error, results) {
+        countyDb.view('selected_counties','county-matchup', {key: [stateFromZip,countyFromZip]}, function(error, results) {
             if (error) console.log("ERROR: Error matching region: " + error);
             results.rows.forEach(function(doc) {
                 console.log("DEBUG: Retrieved this state+county from the view:\n       "
@@ -87,7 +103,7 @@ router.post('/', function(req, res, next) {
                 console.log("DEBUG: You could use this command to verify it:\n       "
                             + "'curl -X GET http://127.0.0.1:5984/selected_counties/"
                             + doc._id + "'\n");
-                if (doc.key[0] == matchedState && doc.key[1] == matchedCounty) {
+                if (doc.key[0] == stateFromZip && doc.key[1] == countyFromZip) {
                     if (matchedRegion) {
                         console.log("DEBUG: another region match found:\n       "
                                     + JSON.stringify(doc) + "\n");
@@ -100,7 +116,7 @@ router.post('/', function(req, res, next) {
             if (matchedRegion) {
                 res.render('thankyou.jade', {region: matchedRegion});
             } else {
-                res.render('sorry.jade', {county: matchedCounty, state: matchedState, zip: zipToSelect});
+                res.render('sorry.jade', {county: countyFromZip, state: stateFromZip, zip: zip_5});
             }
         });
     });
