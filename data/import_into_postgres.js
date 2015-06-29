3c7ec629ae0bef1c47818f96c4c167edc25408fa
Mark Nyon <mark@grandkru.com>
2015-06-29 12:13:56 -0400

    Added script to import data into Postgres using Sequelize & models

---
+++ data/import_into_postgres.js
@@ -0,0 +1,46 @@
+/* - import_into_postgres.js - Node based script that imports JSON files
+ * into Postgres in conjunction with the data store migration from CouchDB,
+ * This script uses the Sequelize ORM (https://github.com/sequelize/sequelize)
+ * for the application models.
+ */
+
+var db = require('./models'); // Sequelize initialization
+var async = require('async'); // Used primarily for iterator
+
+/* import JSON using node require */
+var selectedCountiesJson = require('./data/selected_counties.json');
+var usAddressesJson = require('./data/us_addresses.json');
+
+
+/* Throw an error if the database sync does not occur. The database
+   sync connects to a database and also reinstates the known database
+   structure if for some reason it is not in place */
+db.sequelize.sync().then(function(promise) {
+    console.log("DB in place");
+}).catch(SyntaxError, function(e){
+    console.log("ERROR: Unable to connect to database" + e);
+});
+
+
+/* Import the selected counties from JSON. Create Counties using
+ * Model.create() function from Sequelize.
+ */
+var selectedCounties = db.SelectedCounties;
+
+async.each(selectedCountiesJson.docs, function(county, callback) {
+	selectedCounties.create(county).then(function(returnedCounty) {
+		console.log("County " + returnedCounty + " added");
+	});
+});
+
+
+/* Import the US addresses from JSON. Create UsAddress models using
+ * Model.create() function from Sequelize.
+ */
+var addresses = db.UsAddress;
+
+async.each(usAddressesJson.docs, function(address, callback) {
+	addresses.create(address).then(function(returnedAddress) {
+		console.log("Address " + returnedAddress + " added");
+	});
+});
