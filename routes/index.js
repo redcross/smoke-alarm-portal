767de314869e5c131f586eae0728cee3c1a9f40f
Karl Fogel <kfogel@red-bean.com>
2015-07-15 02:23:43 -0500

    Add missing semicolon.
    Everything worked without this, because semicolons are (mostly)
    optional in Javascript, but our style has been to use them.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('./../models');
-var recipients_table = require(__dirname + '/../config/recipients.json')
+var recipients_table = require(__dirname + '/../config/recipients.json');
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Red Cross' });
