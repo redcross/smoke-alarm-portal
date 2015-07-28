eac15a203c0fec1bed62af4060a4789cb2adc782
Mark Nyon <mark@grandkru.com>
2015-07-28 17:33:20 -0400

    fake request generator for testing: #82
    A short script using faker that generates 725 requests
    for testing purposes

---
+++ data/fake_request_data.js
@@ -0,0 +1,28 @@
+/* fake_request_data.js - Short script to fake requests to make sure
+ * that pagination works as expected. Uses the 'faker' library to create
+ * requests in the admin
+ */
+
+var db = require('./../models');
+var _ = require('underscore');
+var faker = require('faker');
+var requestCount = 725; // TODO: Make configurable
+var requests = [];
+_.times(requestCount, function(index) {
+	var requestData = {
+		name: faker.name.findName(),
+		address: faker.address.streetAddress(),
+		city: faker.address.city(),
+		state: faker.address.state(),
+		zip: faker.address.zipCode(),
+		phone: faker.phone.phoneNumber(),
+		email: faker.internet.email()
+	}
+	requests.push(requestData);
+});
+
+db.Request.sync().then(function () {
+	db.Request.bulkCreate(requests).then(function() {
+		console.log("DEBUG: All " + requestCount + " requests entered");
+	});
+});
