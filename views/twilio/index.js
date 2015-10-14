32b18039c7be68b100c7bd809f6909d70c3d972b
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-10-14 13:39:39 -0500

    Store request data from SMS: #121
    Store the incoming data from SMS's as a request object in the database.
    Add the parse-address module to parse incoming addresses (we'll need to
    test that more, but it works for the simple examples I submitted).
    
    These changes to /views/twilio/index.js make use of a new utilities.js
    file.  This file will ultimately replace most of /views/index.js.  I
    didn't complete the refactoring in this commit, so right now there is a
    fair amount of duplicated code.  I'll clean that up next.
    
    After the cleanup, the next step is to change the contents of our final
    text message to use the new serial number and contact phone number from
    the db.

--- views/twilio/index.js
+++ views/twilio/index.js
@@ -1,6 +1,7 @@
 var env = process.env.NODE_ENV || 'development';
 var config = require(__dirname + '/../../config/config.json')[env];
 var twilio = require('twilio');
+var parser = require('parse-address'); 
 
 var http = require('http'),
     express = require('express'),
@@ -10,16 +11,50 @@ var http = require('http'),
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(cookieParser())
+var serial_num = "example serial";
+var rc_local = "(555)-not-real";
+var client = require('twilio')(config.twilio_accountSid, config.twilio_authToken);
+// array of script responses
+var responses_array = ["Welcome to the smoke alarm request system (para español, texto 'ES'). We need to ask four questions to process your request. Please text back the answer to each and wait for the next question. First, what is your name?", "What is your address, including the unit number, city, state, and zipcode?", "Is the number you're texting from the best way to get in touch with you? If so, text YES. Otherwise, please text a phone number where we can reach you.", "One last question: is there an email address we can use to contact you? If not, text NONE. If yes, please text us the email address.", "Thank you for your smoke alarm request! Your request number is " + serial_num + ". To contact your local Red Cross about this request, call " + rc_local + ". We will be in touch with you to schedule an installation."];
+var request_object = {};
 
+// include the functions from views/index.js
+var save_utils = require('../utilities');
+
+// Generally to find the county and region we use the zipcode, so this
+// takes the entered zip.
+// TODO: it needs to return a serial number and a contact phone number
+// for the region the person is requesting from.  Or, of course, a
+// generic "sorry" if they're out of area or in an inactive region.
+
+var saveRequest = function (zip) {
+    save_utils.findAddressFromZip(zip).then(function(address) {
+        return save_utils.findCountyFromAddress(address, zip);
+    }).then( function(county_id){
+        if (county_id){
+            region_code = county_id.region;
+        }
+        else {
+            region_code = null
+        }
+        request_object.street_address = request_object.address.number + " " + request_object.address.street + " " + request_object.address.type + " " + request_object.address.sec_unit_type + " " + request_object.address.sec_unit_num;
+        request_object.city = request_object.address.city;
+        request_object.state = request_object.address.state;
+        request_object.zip_final = request_object.address.zip;
+        request_object.assigned_rc_region = region_code;
+        return save_utils.countRequestsPerRegion(region_code);
+    }).then( function(numRequests) {
+        requestData = save_utils.createSerial(numRequests, request_object, region_code);
+        return save_utils.saveRequestData(requestData);
+    }).then(function(request) {
+        savedRequest = request;
+        // TODO: construct the final text with the new serial number
+    });
+
+};
+ 
 exports.respond = function(req, res) {
-    // array of script responses
-    var responses_array = ["Welcome to the smoke alarm request system (para español, texto 'ES'). We need to ask four questions to process your request. Please text back the answer to each and wait for the next question. First, what is your name?", "What is your address, including the unit number, city, state, and zipcode?", "Is the number you're texting from the best way to get in touch with you? If so, text YES. Otherwise, please text a phone number where we can reach you.", "One last question: is there an email address we can use to contact you? If not, text NONE. If yes, please text us the email address.", "Thank you for your smoke alarm request! Your request number is <serial number>. To contact your local Red Cross about this request, call <local phone number>. We will be in touch with you to schedule an installation."];
-    // TODO: the last response will need to be changed after we store
-    // the request and get the RC region name, serial number, and other
-    // info from the db.
-
     var twiml = new twilio.TwimlResponse();
-    console.log("DEBUG: " + req.cookies);
     var counter = parseInt(req.cookies.counter) || 0;
 
     // Increment or initialize views, up to the length of our array.  If
@@ -27,16 +62,47 @@ exports.respond = function(req, res) {
     if (counter >= responses_array.length) {
         counter = 0;
     }
+
+    // Use the counter to find out what information is arriving:
+    if (counter == 1) {
+        request_object.name = req.query.Body;
+    }
+    else if (counter == 2) {
+        // then it is their address
+        request_object.address = req.query.Body;
+        request_object.address = parser.parseLocation(request_object.address);
+        // TODO: if we didn't get a zip, add another response in here to
+        // ask for it specifically
+    }
+    else if (counter == 3) {
+        var phone_check = req.query.Body;
+        // handle any capitalization
+        phone_check = phone_check.toLowerCase();
+        
+        if (phone_check == "yes") { 
+            request_object.phone = req.query.From;
+        }
+        else {
+            request_object.phone = req.query.Body;
+        }
+    }
+    else if (counter == 4) {
+        // this is their email address, or none.
+        request_object.email = req.query.Body;
+    }
+
+    // construct a request object and insert it into the db
+    if (request_object.address) {
+        var response_elements = saveRequest(request_object.address.zip);
+    }
+    console.log("DEBUG: " + JSON.stringify(request_object));
+    
     twiml.message(responses_array[counter]);
 
     counter = counter + 1;
     res.cookie('counter',counter);
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
-
-    // TODO: store their incoming texts and construct a request object
-    // from them to insert into the db.  This might be a totally
-    // separate function.
 
 };
 
---
+++ views/utilities.js
@@ -0,0 +1,300 @@
+var db = require('./../models');
+var requestData = {};
+module.exports  = {
+
+    // get count of requests saved for a given region
+    countRequestsPerRegion: function (region) {
+        if (region){
+            return db.Request.count({
+                where: {
+                    assigned_rc_region: region
+                }
+            });
+        }
+        else {
+            return db.Request.count({
+                where: {
+                    assigned_rc_region: null
+                }
+            });
+        }
+    },
+
+    // takes a "value" that needs to be a certain "length" (in this file,
+    // either a date or a sequence number) and pads it with leading zeroes
+    // until it is "length" long.
+    padWithZeroes: function (value, length){
+        while (value.length < length) {
+            value = "0" + value;
+        }
+        return value;
+    },
+
+    findZipForLookup: function (req) {
+        // get zip_for_lookup from req
+        // Treat zip code specially.  For zip codes, we remove all
+        // internal spaces, since they can't possibly be useful.
+        var requestZip = {};
+        requestZip.zip_received = req.body.zip.trim().replace(/\s+/g, '');
+        // This is the zip code we will actually store in the database.
+        // Our canonical form for storing zip codes is any of the following:
+        // "NNNNN" (a 5 digit string), "NNNNN-NNNN" (a string consisting
+        // of 5 digits, a hyphen, and 4 digits), or null.  No other forms
+        // are to be stored, at least not without changing this comment.
+        requestZip.zip_final = null;
+
+        // Parse 5-digit section and optional 4-digit section from the zip code.
+        requestZip.zip_5 = null;
+        requestZip.zip_4 = null;
+        var zip_re = /^([0-9][0-9][0-9][0-9][0-9]) *[-_+]{0,1} *([0-9][0-9][0-9][0-9]){0,1}$/g;
+        requestZip.zip_match = zip_re.exec(requestZip.zip_received);
+        if (requestZip.zip_match) {
+            if (requestZip.zip_match.length < 2) {
+                console.log("ERROR: zip matched, but match grouping is somehow wrong,");
+                console.log("       which implies that the regexp itself is not right");
+                console.log("       (or our use of it isn't right).");
+            }
+            else {
+                requestZip.zip_5 = requestZip.zip_match[1];
+                if (requestZip.zip_match.length == 3 && requestZip.zip_match[2] !== undefined) {
+                    requestZip.zip_4 = requestZip.zip_match[2];
+                    requestZip.zip_final = requestZip.zip_5 + "-" + requestZip.zip_4;
+                } else {
+                    requestZip.zip_final = requestZip.zip_5;
+                }
+            }
+        }
+
+        requestZip.zip_for_lookup = requestZip.zip_5;
+        if (! requestZip.zip_for_lookup) {
+            // If the zip we got doesn't look like it was a real zip, then
+            // it won't work later as a key for database lookups.  But we
+            // should still pass it along so at least error messages can
+            // display it accurately.
+            requestZip.zip_for_lookup = requestZip.zip_received;
+        }
+        
+        return requestZip;
+    },
+
+    getRequestData: function(req, numberOfRequests, region) {
+        var zipArray = findZipForLookup(req);
+        requestData.zip_for_lookup = zipArray.zip_for_lookup;
+        requestData.zip_final = zipArray.zip_final;
+        // Things we derive from the user-provided zip code.
+        var stateFromZip = null;   // remains null if no match
+        var countyFromZip = null;  // remains null if no match
+
+        // Treat state gingerly.  Because of the way ../views/index.js
+        // simulates placeholder text for State, there is a possibility
+        // that, unlike other fields, req.body.state may be undefined.
+        // For other fields we can assume they are strings, either empty
+        // or non-empty, so here we make state meet that assumption too.
+        if (req.body.state === undefined) {
+            req.body.state = '';
+        }
+        // Trim and sanitize the request values.
+        //
+        // Note: we could augment String like so
+        //
+        //     String.prototype.trimAndSlim() {
+        //         return this.trim().replace(/\s+/g, ' ');
+        //     };
+        //
+        // and use that to declutter the code below.  But I'm not sure
+        // whether such augmentation is frowned on or not.  Advice from
+        // more experienced Javascript programmers welcome.  -Karl
+        requestData.name = req.body.name.trim().replace(/\s+/g, ' ');
+        requestData.street_address = req.body.street_address.trim().replace(/\s+/g, ' ');
+        requestData.city = req.body.city.trim().replace(/\s+/g, ' ');
+        requestData.state = req.body.state.trim().replace(/\s+/g, ' ');
+        requestData.phone = req.body.phone.trim().replace(/\s+/g, ' ');
+        requestData.email = req.body.email.trim().replace(/\s+/g, ' ');
+        requestData.assigned_rc_region = region;
+
+
+        return requestData;
+    },
+
+    createSerial: function (numberOfRequests, requestData, region){
+        // construct today date object
+        var today = new Date();
+        // avoid the first request having a serial number of
+        // "region-date-00000."  I think that would be confusing for users.
+        // We might as well start with 1.
+        numberOfRequests = numberOfRequests + 1;
+        var sequenceNumber = module.exports.padWithZeroes(numberOfRequests.toString(), 5);
+        var displayDate = today.getFullYear().toString()+module.exports.padWithZeroes((today.getMonth() +1).toString(), 2) + module.exports.padWithZeroes(today.getDate().toString(), 2);
+        if (region) {
+            var serial = region + "-" + displayDate + "-" + sequenceNumber;
+        }
+        else {
+            // construct code from state
+            var state_code = "";
+            if (requestData.state != ""){
+                // get abbreviation
+                state_code = "XX" + state_abbrevs[requestData.state];
+            }
+            else {
+                state_code = "XXXX";
+            }
+            var serial = state_code + "-" + displayDate + "-" + sequenceNumber;
+        }
+
+        requestData.serial = serial;
+        return requestData;
+    },
+
+    // Save the request data unconditionally.  Even if we can't
+    // service the request -- or even if it contains some invalid
+    // data, such as an unknown zip code -- we still want to record
+    // that the person made the request.
+
+    // TODO: We need to have sanitized all inputs by now.  We need to
+    // know that all input is not problematic from an SQL point of
+    // view (even though we're using an ORM here, we don't want to
+    // store data that will later be a security risk for someone else
+    // generating a report or whatever), and we need to make sure that
+    // the email address does not have surrounding "<" and ">", and
+    // that the phone number is in a standard 10-digit format
+    // (actually, I think we've already validated that, but let's
+    // check again here).
+
+    saveRequestData: function(requestData) {
+        return db.Request.create({
+            name: requestData.name,
+            address: requestData.street_address,
+            city: requestData.city,
+            state: requestData.state,
+            zip: requestData.zip_final,
+            phone: requestData.phone,
+            email: requestData.email,
+            serial: requestData.serial,
+            assigned_rc_region: requestData.assigned_rc_region
+        }).catch( function () {
+            // uniqueness failed; increment serial
+            var serial_array = requestData.serial.split("-");
+            var new_serial = module.exports.padWithZeroes((parseInt(serial_array[2]) + 1).toString(), 5);
+            requestData.serial = serial_array[0] + "-" + serial_array[1] + "-" + new_serial;
+            return saveRequestData(requestData); //loop until save works
+        });
+    },
+
+    // This function gets the address from the zip code in the request
+    findAddressFromZip: function(zip) {
+        return db.UsAddress.findOne({where: {zip: zip}});
+    },
+
+    // This function gets the selected county if it exists from the requests
+    findCountyFromAddress: function(address, zip) {
+        if (!address) {
+            // Then no valid zipcode was found, so make sure that the
+            // "invalid zip" page is displayed
+            requestData.countyFromZip = null;
+            requestData.stateFromZip = null;
+            // make sure that the correct "zip_for_lookup" is specified here...
+            requestData.zip_for_lookup = zip;
+            return null;
+        } 
+
+
+        requestData.countyFromZip = address['county'].replace(" County", "");
+        requestData.stateFromZip = address['state'];
+
+        return db.SelectedCounties.findOne({
+            where: {
+                // Use the PostgreSQL "ILIKE" (case-insensitive LIKE)
+                // operator so that internal inconsistencies in the
+                // case-ness of our data don't cause problems.  For
+                // example, Lac qui Parle County, MN is "Lac qui
+                // Parle" (correct) in ../data/selected_counties.json
+                // but "Lac Qui Parle" (wrong) in us_addresses.json.
+                //
+                // Since us_addresses.json comes from an upstream data
+                // source, correcting all the cases there could be a
+                // maintenance problem.  It's easier just to do our
+                // matching case-insensitively.
+                //
+                // http://docs.sequelizejs.com/en/latest/docs/querying/
+                // has more about the use of operators like $ilike.
+                county: { $ilike: requestData.countyFromZip },
+                state: { $ilike: requestData.stateFromZip }
+            }
+        });
+    },
+
+    // find out whether a region is active or not
+    isActiveRegion: function(request) {
+        return db.activeRegion.findOne({
+            where: {
+                rc_region: request.assigned_rc_region,
+                is_active: true
+            }
+        });
+    },
+
+
+
+
+    // sends an email to the regional representative
+    sendEmail: function(request, selectedRegion) {
+        // selectedRegion is now a row from the activeRegions table
+        var regionPresentableName = selectedRegion.region_name;
+        var regionRecipientName   = selectedRegion.contact_name;
+        var regionRecipientEmail  = selectedRegion.contact_email;
+        var thisRequestID = request.serial;
+
+        var email_text = "We have received a smoke alarm installation request from:\n"
+            + "\n"
+            + "  " + request.name + "\n"
+            + "  " + request.address + "\n"
+            + "  " + request.city + ", " + state_abbrevs[request.state] + "  " + request.zip + "\n";
+
+        if (request.phone) {
+            email_text += "  Phone: " + request.phone + "\n";
+        } else {
+            email_text += "  Phone: ---\n";
+        };
+        if (request.email) {
+            email_text += "  Email: <" + request.email + ">\n";
+        } else {
+            email_text += "  Email: ---\n";
+        };
+
+        email_text += "\n"
+            + "This is installation request #" + thisRequestID + ".\n"
+            + "\n"
+            + "We're directing this request to the administrator for the\n"
+            + "ARC North Central Division, " + regionPresentableName + " region:\n"
+            + "\n"
+            + "  " + regionRecipientName + " <" + regionRecipientEmail + ">\n"
+            + "\n"
+            + "Thank you,\n"
+            + "-The Smoke Alarm Request Portal\n";
+
+        // Send an email to the appropriate Red Cross administrator.
+        var outbound_email = {
+            from: db.mail_from_addr,
+            to: regionRecipientName + " <" + regionRecipientEmail + ">",
+            subject: "Smoke alarm install request from " 
+                + request.name + " (#" + thisRequestID + ")",
+            text: email_text
+        };
+
+
+        db.mailgun.messages().send(outbound_email, function (error, body) {
+            // TODO: We need to record the sent message's Message-ID 
+            // (which is body.id) in the database, with the request.
+            if (body.id === undefined) {
+                console.log("DEBUG: sent mail ID was undefined");
+            } else {
+                console.log("DEBUG: sent mail ID:  '" + body.id + "'");
+            }
+            if (body.message === undefined) {
+                console.log("DEBUG: sent mail msg was undefined");
+            } else {
+                console.log("DEBUG: sent mail msg: '" + body.message + "'");
+            }
+        });
+    }
+}
