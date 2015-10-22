f3a9ab0cb2966b0f7812c4583d74f5fda0a1877d
nttaylor <noeltaylo@gmail.com>
2015-10-22 18:05:18 -0500

    Add Spanish translations #10
    Spanish translations are now in place for index.jade, thankyou.jade,
    sorry.jade, and layout.jade (which contains the "Javascript is disabled"
    message). Note that these translations are present not only for ordinary
    text on the front page, but also for form-field placeholders, form
    validation error messages, and screen-reader tags. The only text left
    untranslated right now is (I think) the value in the html <title> tags
    and the header (<h1>) text on the "thank you" and "sorry" pages. I await
    resolution of #123 before providing translations for the headers.

--- locales/en.js
+++ locales/en.js
@@ -1,4 +1,16 @@
 {
 	"Hello!": "Hello!",
-	"Thank you for your interest in having FREE smoke alarms installed in your home.": "Thank you for your interest in having FREE smoke alarms installed in your home."
+	"Thank you for your interest in having FREE smoke alarms installed in your home.": "Thank you for your interest in having FREE smoke alarms installed in your home.",
+	"To request a FREE installation, please complete all fields in the form below and click “Submit.\"": "To request a FREE installation, please complete all fields in the form below and click “Submit.\"",
+	"Smoke alarms save lives.  Installing a smoke alarm is the first step to keeping your family safe.": "Smoke alarms save lives.  Installing a smoke alarm is the first step to keeping your family safe.",
+	"After submitting your request, you will be contacted by an American Red Cross representative with information on installation availability in your area.": "After submitting your request, you will be contacted by an American Red Cross representative with information on installation availability in your area.",
+	"Your Name": "Your Name",
+	"Address & Unit": "Address & Unit",
+	"Submit": "Submit",
+	"The American Red Cross and its partners have launched the Home Fire Campaign to reduce deaths and injuries caused by home fires by 25 percent over five years.": "The American Red Cross and its partners have launched the Home Fire Campaign to reduce deaths and injuries caused by home fires by 25 percent over five years.",
+	"The Red Cross asks every household in America to take two simple steps to help save lives: check or install smoke alarms and practice fire drills at home.": "The Red Cross asks every household in America to take two simple steps to help save lives: check or install smoke alarms and practice fire drills at home.",
+	"This form can accept requests from the following States and Counties: IA (except Harrison, Mills and Pottawattamie Counties), IL, MN, MO, MT, ND, SD, WI, Dakota County, NE as well as all counties in ID (except Benewah, Bonner, Boundary, Kootenal and Shoshone Counties) and Malheur County, OR and Garfield and Asotin Counties in WA.": "This form can accept requests from the following States and Counties: IA (except Harrison, Mills and Pottawattamie Counties), IL, MN, MO, MT, ND, SD, WI, Dakota County, NE as well as all counties in ID (except Benewah, Bonner, Boundary, Kootenal and Shoshone Counties) and Malheur County, OR and Garfield and Asotin Counties in WA.",
+	"If your location is not listed, please call 800-RED-CROSS to be connected to ": "If your location is not listed, please call 800-RED-CROSS to be connected to ",
+	"your": "your",
+	" local Red Cross Chapter.": " local Red Cross Chapter."
 }
