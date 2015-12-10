df40f30ce4e17c46c103ae00698acea0a6ff25d6
cecilia-donnelly <donnelly.cecilia1@gmail.com>
2015-12-10 16:35:45 -0600

    Remove "select all" checkbox for now: #127
    As mentioned in commit af9dba3, this might be more trouble than it's
    worth at the moment.  The related commits are here in the tree if we
    want to revive this idea.

--- public/views/admin/requests/index.js
+++ public/views/admin/requests/index.js
@@ -148,9 +148,7 @@
     events: {
       'submit form': 'preventSubmit',
       'click input#applyFilters': 'filter',
-      'click input#clearFilters': 'clearFilter',
-      'change input#select_regions': 'selectRegionCheckboxes',
-      'change input[type=checkbox]': 'handleSelectAll'
+      'click input#clearFilters': 'clearFilter'
     },
     endpointDates: {
       'earliest': new Date(2000, 1, 1),
@@ -224,48 +222,13 @@
       if (event.keyCode !== 13) { return; }
       this.filter();
     },
-      selectRegionCheckboxes: function () {
-        // on check/uncheck of "select all," check or uncheck the the
-        // enabled regions
-        if ( $("#select_regions").prop("checked") === true ) {
-            $(".allowed_region input[type=checkbox]").each( function (index) {
-                $(this).prop("checked", "true");
-            });
-        }
-        else {
-            $(".allowed_region input[type=checkbox]").each( function (index) {
-                $(this).prop("checked", false);
-            });
-        }
-      },
-      handleSelectAll: function () {
-          // loop through checkboxes.  if any are unchecked, then
-          // uncheck "select all."  else, check "select all."
-          var uncheck = false;
-          $(".allowed_region input[type=checkbox]").each( function (index) {
-              // but ignore the select all checkbox itself!
-              var id = $(this)[0].id;
-              if ( id == "" && $(this).prop("checked") !== true ) {
-                  uncheck = true;
-              }
-          });
-          if (uncheck) {
-              $("#select_regions").prop("checked", false);
-          }
-          else {
-              $("#select_regions").prop("checked", "true");
-          }
-      },
       filter: function() {
           var query = $('#filters form').serialize();
           // if no checkboxes are checked, then add "no regions" to the query
           if (query.indexOf("region") < 0 ) {
               query = query + "&region%5B%5D=[]";
           }
-          // get value of "select_regions.checked" here and use it again...?
-          var is_checked = $("#select_regions").prop("checked");
           Backbone.history.navigate('q/'+ query, { trigger: true });
-          $("#select_regions").prop("checked", is_checked);
       },
       clearFilter: function () {
           $("#filters form")[0].reset();
