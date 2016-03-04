02d875f0c6608ffd803a28999f5887a79f6c7c85
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-03-04 16:26:05 -0600

    Change abbreviation for new region

--- migrations/20160304-add-seattle.sql
+++ migrations/20160304-add-seattle.sql
@@ -1,44 +1,44 @@
 /* copyright header */
 
-/* Add the Greater Inland Northwest region (GINW) to the active region
+/* Add the Greater Inland Northwest region (NWRN) to the active region
 list. Add counties to Selected Counties */
 
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Idaho', 'Benewah', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Idaho', 'Bonner', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Idaho', 'Boundary', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Idaho', 'Kootenai', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Idaho', 'Shoshone', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Idaho', 'Benewah', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Idaho', 'Bonner', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Idaho', 'Boundary', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Idaho', 'Kootenai', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Idaho', 'Shoshone', now(), now());
 
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Adams', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Benton', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Chelan', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Clallam', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Columbia', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Douglas', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Ferry', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Franklin', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Grant', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Grays Harbor', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Island', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Jefferson', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'King', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Kitsap', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Kittitas', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Lewis', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Lincoln', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Mason', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Okanogan', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Pend Oreille', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Pierce', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'San Juan', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Skagit', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Snohomish', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Spokane', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Stevens', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Thurston', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Walla Walla', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Whatcom', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Whitman', now(), now());
-INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('GINW', 'Washington', 'Yakima', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Adams', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Benton', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Chelan', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Clallam', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Columbia', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Douglas', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Ferry', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Franklin', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Grant', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Grays Harbor', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Island', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Jefferson', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'King', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Kitsap', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Kittitas', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Lewis', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Lincoln', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Mason', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Okanogan', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Pend Oreille', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Pierce', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'San Juan', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Skagit', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Snohomish', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Spokane', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Stevens', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Thurston', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Walla Walla', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Whatcom', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Whitman', now(), now());
+INSERT INTO "SelectedCounties" (region, state, county, "createdAt", "updatedAt") VALUES ('NWRN', 'Washington', 'Yakima', now(), now());
 
 
