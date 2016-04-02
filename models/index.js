5d0ee41da4537cf974349138a04aa8d02b71c318
Jen Trudell <jtrudell@gmail.com>
2016-04-02 11:32:37 -0500

    removes mailgun from db in models/index.js and puts it in views/index.js

--- models/index.js
+++ models/index.js
@@ -1,17 +1,17 @@
 /* Smoke Alarm Installation Request Portal (getasmokealarm.org)
- * 
+ *
  * Copyright (C) 2015  American Red Cross
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,9 +24,6 @@ var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
-var mailgun   = require('mailgun-js')({apiKey: config.mailgun_api_key, 
-                                       domain: config.mail_domain});
-var mail_from_addr = config.mail_from_addr;
 var sequelize = new Sequelize(config.database, config.username, config.password, config);
 var db = {};
 
@@ -66,17 +63,6 @@ db.activeRegion.hasMany(db.regionPermission, {foreignKey:'rc_region'});
 db.SelectedCounties.hasMany(db.Request, {foreignKey:'selected_county'});
 
 db.sequelize.sync(options);
-
-// TODO: This is a temporary kludge.  These shouldn't really be part
-// of the db, and in the long run they won't be.  Once the admin
-// changes are fully integrated, there will be a ../views/index.js
-// file, and we can create the mailgun etc objects there, after line 2
-// where the DB is created.
-//
-// However, that file doesn't exist yet on this branch, so we just
-// stuff this stuff into the db, however inappropriately.
-db.mailgun = mailgun;
-db.mail_from_addr = mail_from_addr;
 
 module.exports = db;
 
