3151cde57caf8f695369fc309518cc4ddeb48748
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-04-02 16:32:34 -0500

    Add API-specific routes file: #121
    Remove CSRF check for the /sms/response endpoint, to resolve #174.
    Create the route before starting to use the CSRF library, so that
    servers outside (e.g. Twilio) can send requests to the endpoint.

--- app.js
+++ app.js
@@ -54,14 +54,16 @@ app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(require('morgan')('dev'));
-app.use(require('compression')());
-app.use(require('serve-static')(path.join(__dirname, 'public')));
-app.use(require('method-override')());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+// keep this before the csrf middleware: #174
+require('./api-routes')(app);
+app.use(require('morgan')('dev'));
+app.use(require('compression')());
+app.use(require('serve-static')(path.join(__dirname, 'public')));
+app.use(require('method-override')());
 app.use(cookieParser(config.cryptoKey));
 app.use(cookieSession({
     resave: true,
