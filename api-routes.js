8c56b46b7e074d897b02dfa85275725c1f8fa926
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-05-12 11:13:02 -0500

    Add endpoint to get active regions: #196
    As requested by @MisterJames, add an endpoint for requesting all the
    current active regions.  I've just included a placeholder for auth here.
    Update the API routes list with the others we'll need for integration
    with AllReady.
    
    Structure responses in accordance with the [Google Style
    Guide](https://google.github.io/styleguide/jsoncstyleguide.xml).

--- api-routes.js
+++ api-routes.js
@@ -1,6 +1,22 @@
 'use strict';
 
 exports = module.exports = function(app) {
+    // Twilio delivery checks
     app.get('/sms/response/', require('./views/sms/response/index').init);
     app.post('/sms/response/', require('./views/sms/response/index').status);
+
+
+    // AllReady integration:
+
+    // list of active regions
+    app.get('/regions/', require('./controllers/regions/index').init);
+
+    // Status updates from AllReady (including confirmation of our initial POST)
+    //app.get('/admin/requests/status/', require('./views/admin/requests/status/index').init);
+    //app.post('/admin/requests/status/', require('./views/admin/requests/status/index').update);
+    
+    // New request list (takes date last polled, returns requests since
+    // then).  We may not need this one, since we'll be sending them
+    // each new request when it comes in.
+    //app.get('/admin/requests/allnew/', require('./views/admin/requests/allnew/index').init);
 }
---
+++ controllers/regions/index.js
@@ -0,0 +1,35 @@
+'use strict';
+var db = require('./../../models');
+
+
+exports.init = function(req, res) {
+    var getAllActiveRegions =  function () {
+        return db.activeRegion.findAll({
+            where: {
+                is_active: true
+            }
+        });
+    }
+    var access_error = [{ "code": "ACCESS_DENIED", "message": "Please pass a valid token to access this content." }];
+    var server_error = [{ "code": "QUERY_PROBLEM", "message": "Sorry, we had a problem finding the regions.  Please try again." }];
+    // TODO: Do a token dance here.  For now, skip it.
+    var token = true;
+    if (token) {
+        var items = [];
+        getAllActiveRegions().then( function (regions) {
+            regions.forEach( function (region) {
+                var basic_region = {"region_id": region.rc_region, "region_name": region.region_name};
+                items.push(basic_region);
+            });
+            var response = {"data": { "items": items}};
+            res.send(response);
+        }).catch(function(error) {
+            var errors = { "error": { "errors": server_error } };
+            res.send(errors);
+        });
+    }
+    else {
+        var errors = { "error": { "errors": access_error } };
+        res.send(errors);
+    }
+};
