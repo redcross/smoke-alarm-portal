80fe962969155b904dd8f6871f1e9e39b84b0553
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2016-05-25 11:50:46 -0500

    Check whether incoming POST is authenticated: #196
    Store auth tokens for external servers that should have permission to
    update status in our database.  Only accept POST requests that have a
    valid token.

--- views/admin/requests/status/index.js
+++ views/admin/requests/status/index.js
@@ -10,35 +10,47 @@ exports.init = function(req, res) {
  * Takes a request with 
  *  - PARAM: id (i.e., serial in our database)
  *  - BODY: Acceptance from AllReady (boolean indicating whether or not they will track this request)
- *    - True usually means this is in a region they are covering
+ *    - True means this is in a region they are covering
  *    - False means this is outside their region
- *  - BODY: Status (one of "new," "in progress," "canceled," "scheduled," and "installed."
+ *  - BODY: Status (one of "new," "in progress," "canceled," "scheduled," and "installed.")
+ *  - BODY: Token (shows that the POST-er has permission to send us information)
+ * 
 */
 exports.update = function(req, res) {
-    // TODO: include auth check
     var access_error = [{ "code": "ACCESS_DENIED", "message": "Please pass a valid token to access this content." }];
     var server_error = [{ "code": "QUERY_PROBLEM", "message": "Sorry, we had a problem updating this region.  Please try again." }];
-    var token = true;
-    if (token) {
-        // update the status of a request
-        return db.Request.findOne(
-            { where:
-              {serial: req.params.id}
-            })
-            .then( function(request) {
-                return request.updateAttributes({
-                    external_tracking: req.body.acceptance,
-                    status: req.body.status
+
+    // get token from req and compare to db
+    var testToken = function () {
+        return db.InboundToken.count({
+            where: {
+                token: req.body.token
+            }
+        });
+    }
+
+    testToken().then( function (token) {
+        if (token > 0) {
+            // update the status of a request
+            return db.Request.findOne(
+                { where:
+                  {serial: req.params.id}
                 })
-            }).then( function (request) {
-                res.send(request.dataValues);
-            }).catch(function() {
-                var errors = { "error": { "errors": server_error } };
-                res.send(errors);
-            });
-    }
-    else {
-        var errors = { "error": { "errors": access_error } };
-        res.send(errors);
-    }
+                .then( function(request) {
+                    return request.updateAttributes({
+                        external_tracking: req.body.acceptance,
+                        status: req.body.status
+                    })
+                }).then( function (request) {
+                    res.send(request.dataValues);
+                }).catch(function() {
+                    var errors = { "error": { "errors": server_error } };
+                    res.send(errors);
+                });
+        }
+        else {
+            var errors = { "error": { "errors": access_error } };
+            res.send(errors);
+        }
+    });
 };
