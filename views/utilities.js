faff4b24e2535aaef60cff99b6f2a73d96add8b2
Kevin Whinnery <kevin.whinnery@gmail.com>
2016-08-06 11:22:09 -0500

    minor fixes for setup experience at givecamp

--- views/utilities.js
+++ views/utilities.js
@@ -379,6 +379,8 @@ module.exports  = {
         db.mailgun.messages().send(outbound_email, function (error, body) {
             // TODO: We need to record the sent message's Message-ID 
             // (which is body.id) in the database, with the request.
+            if (!body) return;
+
             if (body.id === undefined) {
                 console.log("DEBUG: sent mail ID was undefined");
             } else {
