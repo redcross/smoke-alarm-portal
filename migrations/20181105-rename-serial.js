5a929f74fc20723aac7e91b0b35cb75838f8a4df
cecilia-donnelly <cdonnelly@opentechstrategies.com>
2018-11-05 09:28:32 -0600

    Sequelize-format migration to rename serial
    Edit the INSTALL doc because this migration should run correctly as part
    of `sequelize db:migrate`, and won't need to be called out specifically.

---
+++ migrations/20181105-rename-serial.js
@@ -0,0 +1,29 @@
+/* Smoke Alarm Installation Request Portal (getasmokealarm.org)
+ * 
+ * Copyright (C) 2018  American Red Cross
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+'use strict'
+
+// Rename the "serial" column to "public_id" to clarify the code
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+      queryInterface.renameColumn("Requests", "serial", "public_id")
+  },
+  down: function(queryInterface, Sequelize) {
+      queryInterface.renameColumn("Requests", "public_id", "serial")
+  }
+};
