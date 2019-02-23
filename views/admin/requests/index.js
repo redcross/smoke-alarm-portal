75880dc3f5efaebe7a1e7b78a42beceba4bb0702
Frank Duncan <frank@kank.net>
2019-02-23 13:02:20 +0000

    Add page for requests not assigned a region.
    This is basically a duplication of the normal requests page with the
    query hard locked to requests with no linked county in order to double
    check that the system is assigning things correctly.  Try to reuse the
    view code as much as possible with a basic switch flag.
    
    Issue #277: Add County, Chapter Code and Chapter Name

--- views/admin/requests/index.js
+++ views/admin/requests/index.js
@@ -187,8 +187,6 @@ exports.find = function(req, res, next) {
             else {
                 filters.regions = [];
             }
-            // TODO: how do I show requests that aren't linked to a
-            // region (for full-powered admins)?
             return filters;
     });
 }
@@ -348,6 +346,185 @@ var getResults = function(callback) {
     require('async').parallel([countResults, getResults], asyncFinally);
 };
 
+exports.unknown = function(req, res, next) {
+    if (_.isUndefined(req.query.page)) {
+        req.query.page = 1;
+    }
+    if (_.isUndefined(req.query.limit)) {
+        req.query.limit  = 20;
+    }
+    var outcome = {
+        data: null,
+        pages: {
+            current: parseInt(req.query.page, null),
+            prev: 0,
+            hasPrev: false,
+            next: 0,
+            hasNext: false,
+            total: 0
+        },
+        items: {
+            begin: ((req.query.page * req.query.limit) - req.query.limit) + 1,
+            end: req.query.page * req.query.limit,
+            total: 0
+        }
+    };
+    var countResults = function(callback) {
+        var filters = getFilters()
+        req.app.db.Request.count( {
+          where: filters
+        }).then(function(results) {
+            outcome.items.total = results;
+            callback(null, 'done counting');
+        });
+    };
+
+    /*
+     * Return promise object, then return filter object based on request
+     * data from global req.query, suitable for passing to sequelize functions
+     */
+    var getFilters = function() {
+        var filters = { selected_county: null};
+        req.query.search = req.query.search ? req.query.search : '';
+        req.query.limit = req.query.limit ? parseInt(req.query.limit, null) : 20;
+        req.query.page = req.query.page ? parseInt(req.query.page, null) : 1;
+        req.query.sort = req.query.sort ? req.query.sort : '-createdAt';
+        req.query.offset = (req.query.page - 1) * req.query.limit;
+        req.query.startDate = (req.query.startDate) ? moment(req.query.startDate): '';
+        req.query.endDate = (req.query.endDate) ? moment(req.query.endDate) : '';
+        req.query.format = (req.query.format) ? req.query.format : 'json';
+
+        if (req.query.startDate != '' && req.query.endDate != '') {
+            filters.createdAt = {
+                $between:[req.query.startDate.format(), moment(req.query.endDate).endOf("day").format()]
+            };
+        }
+        else {
+            if (req.query.startDate != '') {
+                filters.createdAt = {
+                    gte:req.query.startDate.format()
+                };
+            }
+            else if (req.query.endDate != '') {
+                filters.createdAt = {
+                    lte:moment(req.query.endDate).endOf("day").format()
+                };
+            }
+        }
+        if (req.query.status && req.query.status != 'all') {
+            filters.status = req.query.status;
+        }
+        if (req.query.search) {
+            filters.name = {
+                $ilike: '%' + req.query.search + '%'
+            };
+        }
+
+        return filters;
+    };
+
+    var getResults = function(callback) {
+        var filters = getFilters();
+        // Determine direction for order
+        var sortOrder = (req.query.sort[0] === '-')? 'DESC' : 'ASC';
+        var limit;
+        var offset;
+        if (req.query.format == 'csv') {
+            limit = null;
+            offset = null;
+        }
+        else {
+            limit = req.query.limit;
+            offset = req.query.offset;
+        }
+
+        // Determine whether to filter by date
+        req.app.db.Request.findAll({
+            where: filters,
+            order: [[req.query.sort.replace('-',''), sortOrder ]],
+            limit: limit,
+            include: [req.app.db.RequestDuplicate],
+            offset: offset
+        }).then( function (results_array) {
+            outcome.data = results_array;
+            outcome.pages.total = Math.ceil(outcome.items.total / req.query.limit);
+            outcome.pages.next = ((outcome.pages.current + 1) > outcome.pages.total ? 0 : outcome.pages.current + 1);
+            outcome.pages.hasNext = (outcome.pages.next !== 0);
+            outcome.pages.prev = outcome.pages.current - 1;
+            outcome.pages.hasPrev = (outcome.pages.prev !== 0);
+            if (outcome.items.end > outcome.items.total) {
+                outcome.items.end = outcome.items.total;
+            }
+            outcome.unknown = true;
+
+            outcome.results = results_array.map(function(result) {
+              var duplicateCount = result.RequestDuplicates.length;
+              result = result.toJSON();
+              result.duplicate_count = duplicateCount;
+              return result;
+            });
+            return callback(null, 'done');
+        }).catch(function(err) {
+            console.log("ERROR calling callback: " + err);
+            return callback(err, null);
+        });
+    };
+
+    var createCSV = function() {
+        var fields = [
+          { label: 'id', value: 'public_id' },
+          'name',
+          'address',
+          'city',
+          'state',
+          'zip',
+          'phone',
+          'email',
+          { label: 'date created', value: 'createdAt' },
+          { label: 'date updated', value: 'updatedAt' },
+          'source',
+          { label: 'count duplicates', value: 'duplicate_count' }
+        ]
+        var parser = new json2csv.Parser({ fields });
+        var csv = parser.parse(outcome.results);
+        res.setHeader('Content-Type','application/csv');
+        res.setHeader('Content-Disposition','attachment; filename=smoke-alarm-requests-' + moment().format() + '.csv;');
+        res.send(csv);
+    }
+
+    var asyncFinally = function(err, results) {
+        if (err) {
+            return next(err);
+        }
+
+        // TODO: This section has duplicated logic which can be removed
+        // For some reason, after using a filter, Backbone treats all links
+        // as XHR requests, even if they're not. This works around the
+        // issue for now, but should be fixed moving forward
+        outcome.filters = req.query;
+        if (req.xhr) {
+            if (req.query.format !== "csv") {
+                res.header('Cache-Control', 'no-cache, no-store, must-revalidate');
+                res.send(outcome);
+            } else {
+                createCSV();
+            }
+        } else {
+            outcome.results.filters = req.query;
+            if (req.query.format !== "csv") {
+                res.render('admin/requests/index', {
+                    data: {
+                        csrfToken: res.locals.csrfToken,
+                        results: escape(JSON.stringify(outcome))
+                    }
+                });
+            } else {
+                createCSV();
+            }
+        }
+    };
+    require('async').parallel([countResults, getResults], asyncFinally);
+};
 
 
 exports.read = function(req, res, next) {
